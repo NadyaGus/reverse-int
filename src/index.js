bf05074a7e636e0baecbd11b29ea0fa066de883c
NadyaGus <ftotkexit@gmail.com>
2023-08-10 15:56:03 +0300

    feat: test solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 module.exports = function reverse (n) {
-  // сделать число строкой
-  // сделать реверс строки через массив, как в задаче readeble human number
-  // сделать реверс массив обратно строкой, а потом числом ( посмотреть может сразу можно сделать число);
+  let string = n.toString(); // сделать число строкой
+  let arrayString = string.split(''); // сделала массив 
+  let reverseArray = arrayString.reverse();  // сделала реверс массив
+  let rvsArrayToString = reverseArray.toString(); //сделала реверс массив строчкой
+  let result = rvsArrayToString.replaceAll(',', ''); //результат с заменой запятой из массива
+
+  return result.replaceAll('-', ''); // в тестах выскакивала ошибка с минусом, проще было вставить замену минуса на пустую строчку))
 }
